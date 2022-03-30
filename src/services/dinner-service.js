4ee72aefde3fc05b7c3684ddf91ea4fc5a239b35
Hans Schroeder <hajschroeder@gmail.com>
2022-03-29 20:39:32 -0700

    changes to token authorization

--- src/services/dinner-service.js
+++ src/services/dinner-service.js
@@ -2,8 +2,8 @@
 export default class DinnerService {
   static getFood(searchWord, zip, radius, price) {
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer YMCYA6ba-GH02gCzC3uVIESgkjKvW6WcewGvEtkWA2IgdJfJA7UPOs2gqNtuVksjVfC331oMCoasrAofQibrDf3Qn8G_DqiTMew1NHJJayE1xX28KDwMtfGoVqxAYnYx");
-    myHeaders.append("YMCYA6ba-GH02gCzC3uVIESgkjKvW6WcewGvEtkWA2IgdJfJA7UPOs2gqNtuVksjVfC331oMCoasrAofQibrDf3Qn8G_DqiTMew1NHJJayE1xX28KDwMtfGoVqxAYnYx", "");
+    myHeaders.append("Authorization", `Bearer ${process.env.API_KEY}`);
+    myHeaders.append(`${process.env.API_KEY}`, "");
     myHeaders.append("Cookie", "hl=en_US; wdi=1|483CBA32CEF4A16D|0x1.8902fd6128db5p+30|4e112f406f58471b");
 
     let requestOptions = {
