7f104f897fd851ee1d1fa4188db8a9e9484a12cf
Hans Schroeder <hajschroeder@gmail.com>
2022-03-29 20:28:21 -0700

    change variable name

--- src/services/dinner-service.js
+++ src/services/dinner-service.js
@@ -1,6 +1,6 @@
 
 export default class DinnerService {
-  static getFood(keyword, zip, radius, price) {
+  static getFood(searchWord, zip, radius, price) {
     const myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer YMCYA6ba-GH02gCzC3uVIESgkjKvW6WcewGvEtkWA2IgdJfJA7UPOs2gqNtuVksjVfC331oMCoasrAofQibrDf3Qn8G_DqiTMew1NHJJayE1xX28KDwMtfGoVqxAYnYx");
     myHeaders.append("YMCYA6ba-GH02gCzC3uVIESgkjKvW6WcewGvEtkWA2IgdJfJA7UPOs2gqNtuVksjVfC331oMCoasrAofQibrDf3Qn8G_DqiTMew1NHJJayE1xX28KDwMtfGoVqxAYnYx", "");
@@ -16,7 +16,7 @@ export default class DinnerService {
     // let radius;
     // let sort;
     // let price;
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${keyword}&location=${zip}&radius=${radius}&limit=15&price=${price}`, requestOptions)
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchWord}&location=${zip}&radius=${radius}&limit=15&price=${price}`, requestOptions)
       .then(response => response.json())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
