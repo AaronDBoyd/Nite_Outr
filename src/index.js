c5462b89507535829b841e9954a28885ce83bbda
Todd Pangilinan <pang.todd@gmail.com>
2022-03-30 18:40:37 -0700

    refactor to async/await

--- src/index.js
+++ src/index.js
@@ -5,30 +5,41 @@ import './css/styles.css';
 import DinnerService from './services/dinner-service.js';
 
 function clearFields() {
+  $('#keyWordSearch').val("");
   $('#searchLocation').val("");
+  $('#searchRadius').val("");
+  $('#priceRange').val("");
+  // $('#sortBy').val("");
   $('.showRestaurants').text("");
+  $('.showErrors').text;
 }
 
 function getElements(response) {
-  for (let i = 0; i < response.data.length; i++) {                                           // adjust
-    if (response.data[i].images.downsized.url) {                                               // adjust
-      $('.showRestaurants').append(`<img src="${response.data[i].images.downsized.url}">`);    // adjust
-    } else {
-      $('.showErrors').text(`There was an error processing your request: ${response.message}`);
-    }
-  }}
+  console.log(response);
+  // for (let i = 0; i < response.data.length; i++) {  
+  if (response) {  
+    const grub = (response.businesses[0].name);                                             
+    $('.showRestaurants').append(`${grub}`); 
+  } else {
+    $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+  }
+}
 
 $(document).ready(function() {
   $('#enterSearch').click(function() {
-    const keyword = $('#searchLocation').val();
+    const searchWord = $('#keyWordSearch').val();
+    const zip = $('#searchLocation').val();
+    const radius = $('#searchRadius').val();
+    const price = $('#priceRange').val();
+    // const sortBy = $('#sortBy').val();
+  
     clearFields();
-    DinnerService.getFood(keyword)
+    DinnerService.getFood(searchWord, zip, radius, price)
       .then(function(response) {
         getElements(response);
       });
   });
 });
-
 
 
 
--- src/services/dinner-service.js
+++ src/services/dinner-service.js
@@ -1,14 +1,23 @@
-export default class DinnerService {
-  static getFood() {
-    return fetch(``)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      });
+export default class DinnerService {  
+  static async getFood(searchWord, zip, radius, price) {
+  let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchWord}&location=${zip}&radius=${radius}&limit=50&sort_by=rating&price=${price}`;
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${process.env.API_KEY}`);
+  myHeaders.append(`${process.env.API_KEY}`, "");
+  myHeaders.append("Cookie", "hl=en_US; wdi=1|483CBA32CEF4A16D|0x1.8902fd6128db5p+30|4e112f406f58471b");  
+    try {
+      let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow',
+      };
+      const response = await fetch (url, requestOptions);
+      if (!response.ok){
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
-}
+  }
