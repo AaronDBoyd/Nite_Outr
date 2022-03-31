aafdc76f22edda54227d545680a8a3618be0fbd0
Hans Schroeder <hajschroeder@gmail.com>
2022-03-30 20:39:50 -0700

    include working loop for UI results

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function clearFields() {
   $('#searchLocation').val("");
   $('#searchRadius').val("");
   $('#priceRange').val("");
+  $('#resultsTotal').val("");
   // $('#sortBy').val("");
   $('.showRestaurants').text("");
   $('.showErrors').text;
@@ -16,26 +17,28 @@ function clearFields() {
 
 function getElements(response) {
   console.log(response);
-  // for (let i = 0; i < response.data.length; i++) {  
+  for (let i = 0; i < response.businesses.length; i++) {  
 
-  if (response) {  
-    const grub = (response.businesses[0].name);                                             
-    $('.showRestaurants').append(`${grub}`); 
-  } else {
-    $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+    if (response) {  
+      const grub = [response.businesses[i].name, response.businesses[i].rating, response.businesses[i].location.display_address];     
+      let grubAsString = grub.join(', ');                                        
+      $('.showRestaurants').append(`${grubAsString} <br>`); 
+    } else {
+      $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+    }
   }
 }
-
 $(document).ready(function() {
   $('#enterSearch').click(function() {
     const searchWord = $('#keyWordSearch').val();
     const zip = $('#searchLocation').val();
     const radius = $('#searchRadius').val();
     const price = $('#priceRange').val();
+    const resultsTotal=$('#resultsTotal').val();
     // const sortBy = $('#sortBy').val();
   
     clearFields();
-    DinnerService.getFood(searchWord, zip, radius, price)
+    DinnerService.getFood(searchWord, zip, radius, price, resultsTotal)
       .then(function(response) {
         getElements(response);
       });
--- src/services/dinner-service.js
+++ src/services/dinner-service.js
@@ -1,9 +1,8 @@
 
 export default class DinnerService {
-  static getFood(searchWord, zip, radius, price) {
+  static getFood(searchWord, zip, radius, price, resultsTotal) {
     const myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${process.env.API_KEY}`);
-    myHeaders.append(`${process.env.API_KEY}`, "");
     myHeaders.append("Cookie", "hl=en_US; wdi=1|483CBA32CEF4A16D|0x1.8902fd6128db5p+30|4e112f406f58471b");
 
     let requestOptions = {
@@ -12,7 +11,7 @@ export default class DinnerService {
       redirect: 'follow',
     };
 
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchWord}&location=${zip}&radius=${radius}&limit=50&sort_by=rating&price=${price}`, requestOptions)
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchWord}&location=${zip}&radius=${radius}&limit=${resultsTotal}&sort_by=rating&price=${price}`, requestOptions)
       .then(function(response){
         if(!response.ok){
           throw Error(response.statusText);
