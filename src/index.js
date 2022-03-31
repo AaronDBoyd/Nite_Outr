589e3fc2b631561155cefafddea798fb3b1c02c2
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-30 18:40:13 -0700

    stuff

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ function clearFields() {
 }
 
 function getElements(response) {
-  for (let i = 0; i < response.data.length; i++) {                                           // adjust
-  if (response.data[i].images.downsized.url) {                                               // adjust
-    $('.showRestaurants').append(`<img src="${response.data[i].images.downsized.url}">`);    // adjust
+  // for (let i = 0; i < response.data.length; i++) {  
+                                              
+  if (response) {  
+    const grub = (response.businesses[0].name);                                             
+    $('.showRestaurants').append(`${grub}`); 
+    console.log(response.json());   
   } else {
     $('.showErrors').text(`There was an error processing your request: ${response.message}`);
   }
-}}
+}
 
 $(document).ready(function() {
   $('#enterSearch').click(function() {
--- src/services/dinner-service.js
+++ src/services/dinner-service.js
@@ -1,14 +1,45 @@
 export default class DinnerService {
   static getFood(keyword) {
-    return fetch(``)
-    .then(function(response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
+    // const requestOptions = (
+    //   method: 'get',
+    //   headers: new Headers({
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Authorization': Bearer ZYsYAXWuRmMHfqtSkaXkr8WrDcWqLJmv0RfShLLTZvfedv4JBN00G3g8z0uvOCw3RKExze7c_RiCzKq0NJslf9bmaTSv0MFIZQr0wbsVHNJSouxtnML7gO_L2ERCYnYx
+    //     'Content-Type': 'application/json'
+    // })
+
+  
+    return fetch(`https://api.yelp.com/v3/businesses/search?term=${keyword}&limit=3&location=89129`, 
+    {
+      method: 'GET',
+      headers: new Headers ({
+        // 'Access-Control-Allow-Origin': '*',
+        'Authorization': 'Bearer ZYsYAXWuRmMHfqtSkaXkr8WrDcWqLJmv0RfShLLTZvfedv4JBN00G3g8z0uvOCw3RKExze7c_RiCzKq0NJslf9bmaTSv0MFIZQr0wbsVHNJSouxtnML7gO_L2ERCYnYx',
+        'Content-Type': 'application/json',
+        'Redirect': 'follow'
+
+      })
     })
-    .catch(function(error) {
-      return error;
-    })
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+        
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
+
+
+// const projectHeaders = new Headers();
+//     projectHeaders.append( 'Authorization', `Bearer ZYsYAXWuRmMHfqtSkaXkr8WrDcWqLJmv0RfShLLTZvfedv4JBN00G3g8z0uvOCw3RKExze7c_RiCzKq0NJslf9bmaTSv0MFIZQr0wbsVHNJSouxtnML7gO_L2ERCYnYx`);
+//     projectHeaders.append (`ZYsYAXWuRmMHfqtSkaXkr8WrDcWqLJmv0RfShLLTZvfedv4JBN00G3g8z0uvOCw3RKExze7c_RiCzKq0NJslf9bmaTSv0MFIZQr0wbsVHNJSouxtnML7gO_L2ERCYnYx`, "");
+  
+//     const requestOptions = {
+//       method: 'GET',
+//       headers: this.projectHeaders,
+//       redirect: 'follow'
+//       }
