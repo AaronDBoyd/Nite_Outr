657fddb8fa58a62ed7811e986f187b3da12dab42
Hans Schroeder <hajschroeder@gmail.com>
2022-04-03 16:28:15 -0700

    further changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import 'bootswatch/dist/sketchy/bootstrap.min.css';
+//import 'bootswatch/dist/sketchy/bootstrap.min.css';
 import DinnerService from './services/dinner-service.js';
 
 function clearFields() {
