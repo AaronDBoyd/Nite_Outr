6e90a3788e03762b5981a77cb579084fd3284b34
Todd Pangilinan <pang.todd@gmail.com>
2022-04-05 12:09:13 -0700

    got map working on both dinner results and after dinner activities results.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './css/styles.css';
 import DinnerService from './services/dinner-service.js';
 
 let alpha = ["A","B","C","D","E","F","G","H","I","J"];
-let mapUrl= `https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap&key=${process.env.API_KEY2}`;
+let mapUrl= `https://maps.googleapis.com/maps/api/staticmap?size=512x512&key=${process.env.API_KEY2}`;
 
 function clearFields(){
   $('.showErrors').text("");
@@ -20,13 +20,13 @@ function getElements(response) {
       const grub = [alpha[i]+') '+ response.businesses[i].name, `Rating: ${response.businesses[i].rating}`, response.businesses[i].location.display_address, response.businesses[i].display_phone];     
       let grubAsString = grub.join('<br>');
       const url = response.businesses[i].url;
-      mapUrl=mapUrl+`&markers=color:red%7C${response.businesses[i].coordinates.longitude},${response.businesses[i].coordinates.latitude}%7Clabel:${alpha[i]}`;
+      mapUrl=mapUrl+`&markers=color:red%7Clabel:${alpha[i]}%7C${response.businesses[i].coordinates.latitude},${response.businesses[i].coordinates.longitude}`;
       const urlAsDisplay = (`Click <a  id="link" href =${url}/a> here to learn more`);                                       
       $('.showRestaurants').append(`${grubAsString}<br>${urlAsDisplay}<br><br>`); 
     } if (response.total == 0){
       $('.showBadNews').append(`We're sorry, but nothing matched your search!`);
     }
-    $('.map').append(`<img src= "${mapUrl}>`);
+    $('.map').append(`<img src= "${mapUrl}">`);
     console.log(mapUrl);
   }else {
     $('.showErrors').append(`There was an error processing your request: <br>${response}`);
@@ -42,11 +42,15 @@ function getElementsA(response) {
       const afterDinner = [alpha[i]+') '+ response.businesses[i].name, `Rating: ${response.businesses[i].rating}`, response.businesses[i].location.display_address, response.businesses[i].display_phone];
       let afterDinnerAsString = afterDinner.join('<br>');   
       const urlPlans = response.businesses[i].url;
+      mapUrl=mapUrl+`&markers=color:black%7Clabel:${alpha[i]}%7C${response.businesses[i].coordinates.latitude},${response.businesses[i].coordinates.longitude}`;
       const urlAsDisplayPlans = (`Click <a  id="link" href =${urlPlans}/a> to see what this place is all about!`);                                   
       $('.showActivities').append(`${afterDinnerAsString}<br>${urlAsDisplayPlans}<br><br>`); 
     } if (response.total == 0){
       $('.showBadNewsA').append(`We're sorry, but nothing matched your search!`);
     }
+    $('.map').html("");
+    $('.map').append(`<img src= "${mapUrl}">`);
+    console.log(mapUrl);
   } else {
     $('.showErrorsA').text(`There was an error processing your request: ${response.message}`);
   }  
